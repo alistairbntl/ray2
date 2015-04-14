ba44b32babdd447533f239726bf627563fdad815
Alistair Bentley <alistairbntl@gmail.com>
2015-04-13 20:34:00 -0400

    Draft 1 of sphere.cpp commited by ARB

--- sphere.cpp
+++ sphere.cpp
@@ -11,9 +11,11 @@ using namespace std;
 // Constructor
 sphere_t:: sphere_t(ifstream &infile) : sobj_t(infile, "sphere")
 {
-   /**  STUBBED.  COMPLETE FOR RAY2  **/ 
-
-   load(infile);
+  center = myvector(); // intialize point to <0,0,0>
+  double radius = 0;   // initialize point to 0
+  
+  load(infile);
+  
 }
 
 void sphere_t:: load(ifstream &infile) 
@@ -40,4 +42,55 @@ void sphere_t:: load(ifstream &infile)
    }
 }
 
-/**  IMPLEMENT THE REMAINING METHODS  **/
+myvector sphere_t::getcenter(){
+  myvector my_center ( center.getx(), center.gety(), center.getz());
+  return my_center;
+}
+
+double sphere_t::getradius(){
+
+  return radius;
+
+}
+
+int sphere_t::hits(myvector &base, myvector &dir, hitinfo_t &hit){
+
+  double a,b,c,discriminant,t_h;
+  dir = dir.unitvec();
+  /* sum(const myvector &addend) */
+  /* scale(double fact) */
+  myvector new_base = base + center*-1.0;
+  myvector hit_point;
+
+  // first calculate the values for the quadratic formula
+  a = dir.dot(dir);
+  b = 2*new_base.dot(dir);
+  c = new_base.dot(new_base) - radius*radius;
+
+  discriminant = b*b - 4*a*c;
+
+  if (discriminant <= 0) return 0;
+
+  t_h = (-b - sqrt(discriminant))/(2*a);
+  if (t_h<0) return 0;
+
+  hit_point = base + dir*t_h;
+  if (hit_point.getz() > 0) return 0;
+  
+  hit.sethitpoint(hit_point);
+  myvector normal_temp = hit_point+center*-1;
+  hit.setnormal(normal_temp*normal_temp.length());
+  myvector distance_temp = hit_point*-1 + base;
+  hit.setdistance(distance_temp.length());
+
+  return 0;
+}
+
+void sphere_t::dump(){
+  
+  std::cout << "     center:     ";
+  center.print() ;
+  std::cout << endl;
+  std::cout << "     radius:     " << radius << endl;
+ 
+}
